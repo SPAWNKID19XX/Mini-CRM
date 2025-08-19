7b9a9b5bdeb615b32d952ac4a5e26d6d11cc2684
boris-isac <borisisac@gmail.com>
2025-08-19 14:54:24 +0100

    [ADD] Models descriptions

--- server/minicrm/clients/models.py
+++ server/minicrm/clients/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.template.base import kwarg_re
+from django.template.defaultfilters import lower
+
 
 # Create your models here.
+class Clients(models.Model):
+    name = models.CharField(
+        max_length=256,
+        blank=False,
+        null=False
+    )
+    email = models.CharField(
+        unique=True,
+        max_length=256
+    )
+    phone = models.CharField(
+        null=True,
+        blank=True,
+        max_length=256
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def save(self, *args, **kwargs):
+        if self.email:
+            self.email=self.email.lower()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name} {self.email}'
--- server/minicrm/clients/views.py
+++ server/minicrm/clients/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, HttpResponse
 
 def health_ok(request):
-    print('OK')
     return HttpResponse('OK')
